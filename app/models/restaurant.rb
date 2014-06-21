caebd415f25aa321abfb25e08965ce811d24bbb9
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-06-21 18:27:32 -0400

    added restaurant, review models

---
+++ app/models/restaurant.rb
@@ -0,0 +1,21 @@
+class Restaurant < ActiveRecord::Base
+  has_many :reviews, dependent: :destroy
+  validates :name,
+    presence: true,
+    uniqueness: true
+  validates :address,
+    presence: true,
+    uniqueness: true
+  validates :city,
+    presence: true
+  validates :state,
+    presence: true
+  validates :zipcode,
+    presence: true,
+    length: { is: 5 }
+  validates :description,
+    presence: true
+  validates :category,
+    inclusion: { in: %w(american indian italian thai chinese fusion mexican bbq korean mediterranean asian other),
+      message: "%{value} is not a valid category." }
+end
---
+++ app/models/review.rb
@@ -0,0 +1,18 @@
+class Review < ActiveRecord::Base
+  belongs_to :restaurant
+  validates :rating,
+    presence: true,
+    numericality: { only_integer: true },
+    length: { in: 6..20 }
+  validates :body,
+  presence: true,
+  length: {
+    minimum: 5,
+    maximum: 250,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "A description must have at least %{count} words",
+    too_long: "A description must have at most %{count} words"
+  }
+  validates :restaurant_id,
+    presence: true
+end
